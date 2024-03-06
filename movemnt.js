f3ab62ec37a9372622cb527abb319b35392c7ed8
sjilani1 <syeda_merzia_jilani@brown.edu>
2024-03-06 15:04:53 -0500

    final

---
+++ movemnt.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+    var navList = document.querySelector('.nav-list');
+    navList.style.display === 'none' ? navList.style.display = 'flex' : navList.style.display = 'none';
+  }
